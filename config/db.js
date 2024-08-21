b855aec2b45a46985299090e0b2fae6184e9afa5
disha <dishachhabra@lightningtechnologies.net>
2024-08-21 23:20:04 +0530

    mongo uri removed

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDb = async () => {
   try {
-    const connection = await mongoose.connect("mongodb+srv://admin:12345678cluster@cluster0.gvklmyl.mongodb.net/virtualEvent");
+    const connection = await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to mongoDB ", connection.connection.host);
   } catch (err) {
     console.log(err.message);
