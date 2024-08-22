05a24c8be7cee69580001a35f5eb4e1f9a023a6f
disha <dishachhabra@lightningtechnologies.net>
2024-08-22 19:56:04 +0530

    latest

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import { connectDb } from "./config/db.js";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 const port = 5000;
 
 connectDb();
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
