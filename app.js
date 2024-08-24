23dc29c81118c10bc86b4f92316118e41dd7871a
Avinash-1-10 <avinashyede110@gmail.com>
2024-08-24 10:04:43 +0530

    fix bug

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDb();
-dotenv.config({ path: "./.env.example" });
+dotenv.config({ path: "./.env.dev" });
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
