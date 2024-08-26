5a066c000103559924e500929a62c2156001eb96
disha <dishachhabra@lightningtechnologies.net>
2024-08-26 20:07:02 +0530

    admin,auth,host,organizer middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { connectDb } from "./config/db.js";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
+import middlewareRouter from "./routes/middlewareRouter.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/", middlewareRouter);
 app.use("/api/user", userRouter);
 
 app.listen(port, () => {
