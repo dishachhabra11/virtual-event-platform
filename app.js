25dc73c6b7851c5c5caec388ae84c90891dbdb2d
Avinash-1-10 <avinashyede110@gmail.com>
2024-08-26 22:58:46 +0530

    add create and read api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { connectDb } from "./config/db.js";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
+import eventRouter from "./routes/eventRoutes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/user", userRouter);
+app.use("/api/event", eventRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
---
+++ routes/eventRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createEvent, getAllEvents } from "../controllers/eventController.js";
+import { upload } from "../middlewares/multer.js";
+
+
+const router = express.Router();
+
+router.get("/", getAllEvents);
+router.post("/",upload.single('image'), createEvent);
+
+export default router;
