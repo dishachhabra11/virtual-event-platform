bef31f08626adb2ddb0f4798612869cfe42785aa
disha <dishachhabra@lightningtechnologies.net>
2024-08-27 22:29:04 +0530

    commit

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -2,10 +2,9 @@ import express from "express";
 import { createEvent, getAllEvents } from "../controllers/eventController.js";
 import { upload } from "../middlewares/multer.js";
 
-
 const router = express.Router();
 
 router.get("/", getAllEvents);
-router.post("/",upload.single('image'), createEvent);
+router.post("/", upload.single("image"), createEvent);
 
 export default router;
