3314c6dd915cf3a4a73421e4577253d8fd179616
disha <dishachhabra@lightningtechnologies.net>
2024-08-27 23:22:14 +0530

    commit

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,6 +1,6 @@
-import { cloudinary } from '../config/cloudinary.js';
-import fs from 'fs';
-import Event from '../models/eventModel.js';
+import { cloudinary } from "../config/cloudinary.js";
+import fs from "fs";
+import Event from "../models/eventModel.js";
 
 export const getAllEvents = async (req, res) => {
   try {
@@ -12,64 +12,48 @@ export const getAllEvents = async (req, res) => {
 };
 
 export const createEvent = async (req, res) => {
-    try {
-      const {
-        title,
-        description,
-        date,
-        time,
-        location,
-        genre,
-        price,
-        link,
-        duration,
-        language,
-        ageRestriction,
-        availableSeats,
-        performers,
-        creator,
-        tags,
-      } = req.body;
-  
-      let imageUrl;
-      if (req.file) {
-        const result = await cloudinary.uploader.upload(req.file.path, {
-          folder: 'uploads',
-        });
-  
-        fs.unlinkSync(req.file.path);
-        imageUrl = result.secure_url;
-      }
-  
-      const newEvent = new Event({
-        title,
-        description,
-        date,
-        time,
-        location,
-        genre,
-        price,
-        link,
-        duration,
-        language,
-        ageRestriction,
-        availableSeats,
-        performers,
-        creator,
-        tags,
-        image: imageUrl || '',
+  try {
+    const { title, description, date, time, location, genre, price, link, duration, language, ageRestriction, availableSeats, performers, creator, tags } = req.body;
+
+    let imageUrl;
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "uploads",
       });
-  
-      await newEvent.save();
-  
-      res.status(201).json({
-        message: 'Event created successfully',
-        data: newEvent,
-      });
-    } catch (error) {
-      res.status(500).json({
-        message: 'Failed to create event',
-        error: error.message,
-      });
+
+      fs.unlinkSync(req.file.path);
+      imageUrl = result.secure_url;
     }
-  };
+
+    const newEvent = new Event({
+      title,
+      description,
+      date,
+      time,
+      location,
+      genre,
+      price,
+      link,
+      duration,
+      language,
+      ageRestriction,
+      availableSeats,
+      performers,
+      creator,
+      tags,
+      image: imageUrl || "",
+    });
+
+    await newEvent.save();
+
+    res.status(201).json({
+      message: "Event created successfully",
+      data: newEvent,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to create event",
+      error: error.message,
+    });
+  }
+};
