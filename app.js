a0700ca87fceb419eccecd62043a6be35e55d563
Avinash-1-10 <avinashyede110@gmail.com>
2024-09-15 14:19:31 +0530

    Add Google Login

--- app.js
+++ app.js
@@ -4,20 +4,28 @@ import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
 import middlewareRouter from "./routes/middlewareRouter.js";
 import eventRouter from "./routes/eventRoutes.js";
+import googleAuthRouter from "./routes/googleAuthRoutes.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import setupPassport from "./services/passport.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDb();
 dotenv.config({ path: "./.env.dev" });
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
 app.use(cookieParser());
+setupPassport(app);
 
-app.use("/", middlewareRouter);
+// app.use("/", middlewareRouter);
 app.use("/api/user", userRouter);
-app.use("/api/event", eventRouter)
+app.use("/api/event", eventRouter);
+app.use('/auth', googleAuthRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
